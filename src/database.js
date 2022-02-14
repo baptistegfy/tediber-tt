cceafb1ddba1a744f420e2fa49b4e101cf9c4159
baptiste geffroy <baptiste@MacBook-Pro-de-baptiste.local>
2022-02-14 17:38:30 +0100

    add svg and set a fake product db

---
+++ src/database.js
@@ -0,0 +1,33 @@
+const product = [
+  {
+  name: '\'incroyable matelas tediber',
+  img: 'Tediber_mattress.jpg',
+  price: '750',
+  price_unit: '€',
+  width: '160',
+  length: '200',
+  size_unit:'cm',
+  quantity: '1'
+  },
+  {
+    name: '\'incroyable oreiller tediber',
+    img: 'Tediber_pillow.jpg',
+    price: '85',
+    price_unit: '€',
+    width: '60',
+    length: '60',
+    size_unit:'cm',
+    quantity: '1'
+  },
+  {
+  name: '\'incroyable couette tediber',
+    img: 'Tediber_duvet.jpg',
+    price: '210',
+    price_unit: '€',
+    width: '240',
+    length: '220',
+    size_unit:'cm',
+    quantity: '1'
+  }
+]
+  module.exports = product;
